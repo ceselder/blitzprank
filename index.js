3b199e1744f417655d9fc5b518665ea1aba61e0d
celestedeschamphelaere@github.com <celestedeschamphelaere@gmail.com>
2022-09-09 11:39:41 +0200

    browser path

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ async function takeUggScreenShot(url)
     const filename = `${crypto.randomBytes(20).toString('hex')}.png`;
     const padding = 20;
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      executablePath: '/usr/bin/chromium-browser'
+    })
     let page = await browser.newPage();
     await page.setViewport({ width: 1920, height: 1080})
     await page.goto(url);
